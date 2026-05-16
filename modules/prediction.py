b3ce57142e46b5070b1a8e696c994fd2e4db0b0e
unknown <sharma2004223@gmail.com>
2026-05-16 16:30:26 +0530

    PREDICTION MODEL UPDATION

--- modules/prediction.py
+++ modules/prediction.py
@@ -2,20 +2,46 @@ import pickle
 import os
 import numpy as np
 
-model_path = "models/risk_model.pkl"
+# -----------------------------------
+# GET BASE DIRECTORY
+# -----------------------------------
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# -----------------------------------
+# MODEL PATH
+# -----------------------------------
+
+model_path = os.path.join(
+    BASE_DIR,
+    "..",
+    "models",
+    "risk_model.pkl"
+)
+
+# -----------------------------------
+# LOAD MODEL
+# -----------------------------------
+
+model = None
 
 if os.path.exists(model_path):
-    model = pickle.load(open(model_path, "rb"))
-else:
-    model = None
 
+    with open(model_path, "rb") as file:
+        model = pickle.load(file)
+
+# -----------------------------------
+# PREDICT FUNCTION
+# -----------------------------------
 
 def predict_risk(attendance, internal, participation, absences, gpa):
 
     if model is None:
         return "Model not loaded"
 
-    data = np.array([[attendance, internal, participation, absences, gpa]])
+    data = np.array([
+        [attendance, internal, participation, absences, gpa]
+    ])
 
     prediction = model.predict(data)
 
